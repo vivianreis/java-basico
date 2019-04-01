cd729ad6224b7b914a325cf7dfc3b617c964071d
Vivian Barbosa Reis <srtvivian@hotmail.com>
2019-04-01 14:34:50 -0300

    atualização java herança

---
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,15 @@
+
+public class TesteFuncionario {
+	
+	public static void main(String[] args) {
+		
+		Funcionario nico = new Funcionario();
+		nico.setNome("Nico Steppat");
+		nico.setCpf("2223334446-9");
+		nico.setSalario(2600.00);
+		
+		System.out.println(nico.getNome());
+		System.out.println(nico.getBonificacao());	
+	}
+
+}
