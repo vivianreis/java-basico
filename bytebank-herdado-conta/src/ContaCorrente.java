f8b9ac6c99025e261e4d95a7316ebd33a5b2ef91
Vivian Barbosa Reis <srtvivian@hotmail.com>
2019-04-02 17:10:04 -0300

    Alura JAVA: Atualizando aula de herença e o uso de construtores

---
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -0,0 +1,17 @@
+
+
+//new ContaCorrente();
+public class ContaCorrente extends Conta {
+	
+	public ContaCorrente (int agencia, int numero) {
+		super(agencia, numero);
+	}
+	
+	@Override
+	public boolean saca(double valor) {
+		// TODO Auto-generated method stub
+		double valorASacar = valor + 0.2;
+		return super.saca(valorASacar);
+	}
+
+}
